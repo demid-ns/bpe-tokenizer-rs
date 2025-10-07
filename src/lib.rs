9d915d9d8cb0ebbfb365445e72a5c3a25596312d
demid-ns <46938257+demid-ns@users.noreply.github.com>
2025-10-07 17:54:04 +0300

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
+mod byte_encoder;
 pub mod encoder;
 mod symbols;
 mod test_utils;
-mod byte_encoder;
 pub mod trainer;
 
+pub use byte_encoder::{bytes_to_unicode, unicode_to_bytes};
 pub use encoder::Encoder;
 use symbols::word_to_symbols;
 pub use trainer::Trainer;
-pub use byte_encoder::{bytes_to_unicode, unicode_to_bytes};
